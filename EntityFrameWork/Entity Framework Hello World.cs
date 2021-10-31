434b859fe905c694abeb47392664f26b7cb4e7f6
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-10-31 11:48:30 +0200

    31/10

--- EntityFrameWork/Entity Framework Hello World.cs
+++ EntityFrameWork/Entity Framework Hello World.cs
@@ -27,26 +27,49 @@ namespace EntityFrameWork
     class MeContext : DbContext
     {
         public MeContext() : base(@"Data Source=.;Initial Catalog=FirstDB;Integrated Security=True") { }
-        public DbSet<Video> Viedos{get;set;}
-        public DbSet<PlayList> PlayList { get; set; }
 
-    }
+        //the pipeline to the database any things that get added here represent tables
+        public DbSet<Video> Videos{get;set;}
+        public DbSet<PlayList> PlayList { get; set; } 
+
+    } 
     class Entity_Framework_Hello_World
     {
 
         public static void run()
         {
+            var db = new MeContext();
+            //db.Videos.Select(v => v.Title).ToList().ForEach(Console.WriteLine);
+            //return;
+            //by diffult the entity framework doesnot perform the join untill you till it to ,because you may
+            //not need the data from the other table (lazy loading)
+            PlayList thelist = db.PlayList.Include(list => list.PlayListVideo).Single();
+            foreach (var video in thelist.PlayListVideo)
+            {
+                Console.WriteLine(video.Title);
+            }
+            return;
+            db.Videos.RemoveRange(db.Videos);
+            db.PlayList.RemoveRange(db.PlayList);
             Video v1 = new Video
             {
                 Title = "hello entity",
                 Description = "ooooh"
             };
-            var mycontext = new MeContext();
-            mycontext.Viedos.Add(v1);
+            Video v2 = new Video
+            {
+                Title = "hello entity 2",
+                Description = "ooooh2"
+            };
+
+            db.Videos.Add(v1);
+            db.Videos.Add(v2);
+
             PlayList playlist = new PlayList();
             playlist.Title = "first play list";
-            playlist.PlayListVideo.Add(v1);
-            mycontext.SaveChanges();
+            playlist.PlayListVideo = new List<Video> { v1,v2 };
+            db.PlayList.Add(playlist);
+            db.SaveChanges();
             
         }
 
