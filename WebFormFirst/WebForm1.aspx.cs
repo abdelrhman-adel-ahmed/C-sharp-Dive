4de1bfc29c3e83bb8e16564512cda257acfb9d05
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-05 05:32:05 +0200

    5/11

---
+++ WebFormFirst/WebForm1.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormFirst
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        int counter=0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            TextBox1.Text = "0";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            counter = counter + 1;
+            TextBox1.Text = counter.ToString();
+
+        }
+    }
+}
