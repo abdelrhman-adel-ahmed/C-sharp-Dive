7983cce63740d7e11619105f3e443c42c4401faf
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-05 21:31:23 +0200

    5/11

---
+++ ADO/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ADO.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="212px">
+            <Columns>
+                <asp:BoundField />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ ADO/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //adding Connection Timeout solve the timeout problem
+            SqlConnection conn = new SqlConnection("Data Source=.;Integrated Security = SSPI; Initial Catalog = Northwind;Connection Timeout=30;");
+            SqlCommand cmd = new SqlCommand("select top 5 customerid,companyname,contactname,contacttitle from customers", conn);
+            conn.Open();
+            SqlDataReader rdr= cmd.ExecuteReader();
+            GridView1.DataSource = rdr;
+            GridView1.DataBind();
+            conn.Close();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
