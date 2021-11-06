725f97d7d76bfc913dcfa6789237528ed0acff88
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 10:51:46 +0200

    6/11

--- ADO/sqlinjection.aspx.cs
+++ ADO/sqlinjection.aspx.cs
@@ -21,8 +21,12 @@ namespace ADO
             string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
             using(SqlConnection conn = new SqlConnection(cs))
             {
-                string commnad = "select * from videos where title like '" +TextBox1.Text+"%'";
+                //string commnad = "select * from videos where title like '" +TextBox1.Text+"%'";
+                string commnad = "select * from videos where title like @titlename";
+                
                 SqlCommand cmd1 = new SqlCommand(commnad, conn);
+                cmd1.CommandText = commnad;
+                cmd1.Parameters.AddWithValue("@titlename", TextBox1.Text);
                 conn.Open();
                 
                 GridView1.DataSource = cmd1.ExecuteReader();
