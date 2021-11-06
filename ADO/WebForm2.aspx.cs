683963a7714a4c03d3494d5709bed8aa416c7c60
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 07:47:07 +0200

    6/11

---
+++ ADO/WebForm2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace ADO
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string s = ConfigurationManager.ConnectionStrings["Cs"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(s))
+            {
+                SqlCommand cmd = new SqlCommand("select top 5 customerid,companyname,contactname,contacttitle from customers", conn);
+                conn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
