1491c9fa810508130771e0b7605819370402c68b
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 11:04:51 +0200

    6/11

--- ADO/storedProcedure.aspx.cs
+++ ADO/storedProcedure.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ADO
 {
@@ -12,6 +14,37 @@ namespace ADO
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                //procedure name
+                cmd.CommandText = "spAddEmployee";
+                //by defult command type is text but if we want stroedprocedure we have to explicitly ask for it
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", txtEmployeeName.Text);
+                cmd.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+                cmd.Parameters.AddWithValue("@Salary", txtSalary.Text);
+
+                //empid output paramater that the procedure will reutrn 
+                SqlParameter outParameter = new SqlParameter();
+                outParameter.ParameterName = "@EmployeeId";
+                outParameter.SqlDbType = System.Data.SqlDbType.Int;
+                outParameter.Direction = System.Data.ParameterDirection.Output;
+                cmd.Parameters.Add(outParameter);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+
+                string EmpId = outParameter.Value.ToString();
+                lblMessage.Text = $"Employee ID = {EmpId}";
+
+            }
         }
     }
 }
