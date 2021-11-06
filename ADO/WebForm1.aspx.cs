6bd571324be4da21d0d82db51b471b3507ab1bda
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 06:52:52 +0200

    6/11

--- ADO/WebForm1.aspx.cs
+++ ADO/WebForm1.aspx.cs
@@ -14,12 +14,25 @@ namespace ADO
         {
             //adding Connection Timeout solve the timeout problem
             SqlConnection conn = new SqlConnection("Data Source=.;Integrated Security = SSPI; Initial Catalog = Northwind;Connection Timeout=30;");
-            SqlCommand cmd = new SqlCommand("select top 5 customerid,companyname,contactname,contacttitle from customers", conn);
-            conn.Open();
-            SqlDataReader rdr= cmd.ExecuteReader();
-            GridView1.DataSource = rdr;
-            GridView1.DataBind();
-            conn.Close();
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select top 5 customerid,companyname,contactname,contacttitle from customers", conn);
+                conn.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                GridView1.DataSource = rdr;
+                GridView1.DataBind();
+            }
+            catch 
+            {
+
+                throw;
+            }
+            finally
+            {
+                //if any exception occur , then we have to make sure that the connection closed 
+                conn.Close();
+
+            }
         }
 
         protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
