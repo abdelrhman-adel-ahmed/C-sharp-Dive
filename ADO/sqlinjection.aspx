553c0fbfc966c3d599f61c9ada47559b91525f55
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 08:26:32 +0200

    6/11

---
+++ ADO/sqlinjection.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sqlinjection.aspx.cs" Inherits="ADO.sqlinjection" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="height: 26px" Text="Button" />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ ADO/sqlinjection.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ADO
+{
+    public partial class sqlinjection : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using(SqlConnection conn = new SqlConnection(cs))
+            {
+                string commnad = "select * from videos where title like '" +TextBox1.Text+"%'";
+                SqlCommand cmd1 = new SqlCommand(commnad, conn);
+                conn.Open();
+                
+                GridView1.DataSource = cmd1.ExecuteReader();
+                GridView1.DataBind();
+            }
+
+        }
+    }
+}
