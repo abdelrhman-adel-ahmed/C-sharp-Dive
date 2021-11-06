739d04e3dd1cea6011e89dd8cf4d87c693ccb46a
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 12:29:11 +0200

    6/11

--- ADO/datareader.aspx.cs
+++ ADO/datareader.aspx.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data.SqlClient;
+using System.Configuration;
 namespace ADO
 {
     public partial class datareader : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string cs = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlCommand command = new SqlCommand("select * from employees", conn);
+                conn.Open();
+                SqlDataReader dr = command.ExecuteReader();
+                Response.Write(dr);
+            }
         }
     }
 }
