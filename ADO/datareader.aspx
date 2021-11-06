dcf0d942d767621a4b6ca55a5b49bdd087f7f6b7
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-06 13:22:33 +0200

    6/11

--- ADO/datareader.aspx
+++ ADO/datareader.aspx
@@ -10,6 +10,8 @@
     <form id="form1" runat="server">
         <div>
         </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
     </form>
 </body>
 </html>
--- ADO/datareader.aspx.cs
+++ ADO/datareader.aspx.cs
@@ -6,19 +6,40 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 namespace ADO
 {
     public partial class datareader : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string cs = ConfigurationManager.ConnectionStrings[0].ConnectionString;
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
             using (SqlConnection conn = new SqlConnection(cs))
             {
-                SqlCommand command = new SqlCommand("select * from employees", conn);
+                SqlCommand command = new SqlCommand("select * from tblemployees", conn);
                 conn.Open();
                 SqlDataReader dr = command.ExecuteReader();
-                Response.Write(dr);
+                
+                DataTable table = new DataTable();
+                table.Columns.Add("employeeid");
+                table.Columns.Add("name");
+                table.Columns.Add("gender");
+                table.Columns.Add("salary");
+                while (dr.Read())
+                {
+                    DataRow row = table.NewRow();
+                    row["employeeid"] = dr["employeeid"];
+                    row["name"] = dr["name"];
+                    row["gender"] = dr["gender"];
+                    row["salary"] = dr["salary"];
+                    Response.Write(row["name"].ToString()+ "<br/>");
+                    //as we said datareader is forward only and once we consume the row we cannot get it again 
+                    //so we store each row in the table object
+                    table.Rows.Add(row);
+                }
+                GridView1.DataSource = table;
+                GridView1.DataBind();
+                
             }
         }
     }
