9ec6b5697620108c85274030a73ace3ecd21ffc9
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-07 16:12:48 +0200

    7/11

--- WebFormFirst/usercontrol.aspx.cs
+++ WebFormFirst/usercontrol.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace WebFormFirst
 {
@@ -11,19 +6,19 @@ namespace WebFormFirst
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void Button1_Click1(object sender, EventArgs e)
         {
             //if we want to access somthing in the calenderControl we can make properites  
             Response.Write(calenderControl.SelectedDate);
-            
+
         }
     }
 }
---
+++ WebFormFirst/usercontrols/CalenderVisabilityChangedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormFirst.usercontrols
+{
+    public class CalenderVisabilityChangedEventArgs:EventArgs
+    {
+        private bool _isvisible;
+        public CalenderVisabilityChangedEventArgs(bool isvisible)
+        {
+            this._isvisible = isvisible;
+        }
+        public bool IsVisibility
+        {
+            get { return _isvisible; }
+        }
+    }
+
+    public delegate void CalenderVisibilityChangedEventHandler(object sender, CalenderVisabilityChangedEventArgs e);
+}
