f0eb70c51da24cf6f65ee491a2ef4626af87671b
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-08 11:37:26 +0200

    8/11

---
+++ ADO/cachingDataSets.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cachingDataSets.aspx.cs" Inherits="ADO.cachingDataSets" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="LoadData" runat="server" OnClick="LoadData_Click" Text="LoadData" />
+            <asp:Button ID="ClearCach" runat="server" OnClick="ClearCach_Click" Text="ClearCach" />
+            <br />
+            <br />
+            <asp:Label ID="lblMessage" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ADO/cachingDataSets.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+namespace ADO
+{
+    public partial class cachingDataSets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoadData_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from tblemployees", conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                Cache
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void ClearCach_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
