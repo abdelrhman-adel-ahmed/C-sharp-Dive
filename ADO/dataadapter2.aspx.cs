dd72437dafff062387e7de335a27b0a927ca231e
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-08 10:46:50 +0200

    8/11

---
+++ ADO/dataadapter2.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+namespace ADO
+{
+    public partial class dataadapter2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from videos;select * from tblemployees", cs);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds.Tables[0];
+                GridView1.DataBind();
+                GridView2.DataSource = ds.Tables[1]; ;
+                GridView2.DataBind();
+
+            }
+        }
+    }
+}
