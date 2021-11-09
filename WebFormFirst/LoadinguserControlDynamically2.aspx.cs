8ad2846c339c231be97b0503dec9e29b02b06ca1
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-09 09:39:31 +0200

    9/11

--- WebFormFirst/LoadinguserControlDynamically2.aspx.cs
+++ WebFormFirst/LoadinguserControlDynamically2.aspx.cs
@@ -11,28 +11,43 @@ namespace WebFormFirst
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //problem with this approach that we save unnecessary data in the viewstate so we have additional 
+            //bytes get saved in the view state ,if you open the trace you wil see the droplist and the text
+            //box is saved beacuse we create them but we set the visiabliry to false ,also we have anothe problem
+            //which is we have to do all this work despite we will not use all of those controls 
+            //how we can solve that ?!!
 
-        }
+            //create dropdownlist control and then 
+            DropDownList DDL1 = new DropDownList();
+            DDL1.ID = "DDL1";
+            DDL1.Items.Add("cairo");
+            DDL1.Items.Add("minia");
+            DDL1.Items.Add("zrbo");
+            DDL1.Items.Add("elno");
+            PlaceHolder1.Controls.Add(DDL1);
+            DDL1.Visible = false;
 
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
+
+            TextBox text1 = new TextBox();
+            text1.Text = DropDownList1.SelectedValue;
+            Panel1.Controls.Add(text1);
+            text1.Visible = false;
+
+
             if (DropDownList1.SelectedValue == "DDL")
             {
-                //create dropdownlist control and then 
-                DropDownList DDL1 = new DropDownList();
-                DDL1.ID = "DDL1";
-                DDL1.Items.Add("cairo");
-                DDL1.Items.Add("minia");
-                DDL1.Items.Add("zrbo");
-                DDL1.Items.Add("elno");
-                PlaceHolder1.Controls.Add(DDL1);
+                DDL1.Visible = true;
+
             }
             else if (DropDownList1.SelectedValue == "TB")
             {
-                TextBox text1 = new TextBox();
-                text1.Text = DropDownList1.SelectedValue;
-                Panel1.Controls.Add(text1);
+                text1.Visible = true;
+            }
+        }
+            protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+            {
+                //problem if we put the code here that any other post back the custom controls will not be 
+                //rendered beause it only render when the select changed event happen
             }
         }
     }
-}
