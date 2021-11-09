068c8497316588a5add257e79693328e5ba3c660
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-09 12:41:18 +0200

    9/11

---
+++ CustomeServerControls/CustomCalender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+
+namespace CustomeServerControls
+{
+    [ToolboxData("<{0}:CustomCalender runat=server></{0}:CustomCalender>")]
+    public class CustomCalender:CompositeControl
+    {
+        TextBox textBox;
+        ImageButton imageButton;
+        Calendar calendar;
+
+        protected override void CreateChildControls()
+        {
+            textBox = new TextBox();
+            textBox.ID = "dateTextBox";
+            textBox.Width = Unit.Pixel(80);
+
+            imageButton = new ImageButton();
+            imageButton.ID = "calenderImageButton";
+
+            calendar = new Calendar();
+            calendar.ID = "calenderControl";
+
+            this.Controls.Add(textBox);
+            this.Controls.Add(imageButton);
+            this.Controls.Add(calendar);
+        }
+        protected override void Render(HtmlTextWriter writer)
+        {
+            textBox.RenderControl(writer);
+            imageButton.RenderControl(writer);
+            calendar.RenderControl(writer);
+
+        }
+    }
+}
---
+++ WebFormFirst/UsingCustomServerControls.aspx
@@ -0,0 +1,19 @@
+﻿
+<%@ Register Assembly="CustomeServerControls" Namespace="CustomeServerControls" TagPrefix="cc1" %>
+<!DOCTYPE html>
+
+<%@ Page language="C#" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <cc1:CustomCalender ID="CustomCalender1" runat="server" />
+        </div>
+    </form>
+</body>
+
+</html>
