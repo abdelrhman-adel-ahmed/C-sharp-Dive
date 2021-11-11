01c76865d58fcf330592527c1fc9712b961ecd10
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-11 11:10:19 +0200

    11/11

---
+++ WebFormFirst/ControlCachingInCode.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ControlCachingInCode.aspx.cs" Inherits="WebFormFirst.ControlCachingInCode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <br />
+            Server Time: 
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            Client Time:
+            <script>
+            document.write(Date());
+            </script>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormFirst/ControlCachingInCode.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebFormFirst
+{
+    public partial class ControlCachingInCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //get all properties of the response object
+            // Response.GetType().GetProperties().ToList().ForEach(x=>Response.Write(x.Name +"<br/>"));
+
+            //get all properties of the Cache object
+            //Response.Cache.GetType().GetProperties().ToList().ForEach(x => Response.Write(x.Name + "<br/>"));
+
+            //get all methods of the Cache object
+            Response.Cache.GetType().GetMethods().ToList().ForEach(x => Response.Write(x.Name + "<br/>"));
+
+            //<%@ OutputCache Duration="30" VaryByParam="DropDownList1" Location="Server"%>
+            //OutputCache but using the Cache proprties and methods 
+            Response.Cache.SetExpires(DateTime.Now.AddSeconds(30));
+            Response.Cache.VaryByParams["None"] = true;
+            Response.Cache.SetCacheability(HttpCacheability.Server);
+
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from videos", conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
