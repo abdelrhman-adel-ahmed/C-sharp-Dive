516e4d53cc0b66389b2efe0da78659b448877093
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-13 11:29:48 +0200

    13/11

---
+++ WebFormFirst/Cache dependency on sql server database table.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cache dependency on sql server database table.aspx.cs" Inherits="WebFormFirst.Cache_dependency_on_sql_server_database_table" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Button ID="Button1" runat="server" Text="Get Data" OnClick="Button1_Click" />
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ WebFormFirst/Cache dependency on sql server database table.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Caching;
+namespace WebFormFirst
+{
+    public partial class Cache_dependency_on_sql_server_database_table : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn= new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("select * from videos", conn);
+               
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+
+                Cache.Insert("videos", ds, null, DateTime.Now.AddSeconds(20), Cache.NoSlidingExpiration,
+                    CacheItemPriority.Default, null);
+                Label1.Text = "data retrived from db at " + DateTime.Now.ToString();
+            }
+        }
+    }
+}
