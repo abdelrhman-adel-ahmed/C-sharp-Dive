aae9f400d5b85253d53a000013d8d91ac0e048cf
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-13 07:51:02 +0200

    13/11

---
+++ WebFormFirst/CachDependencyOnFiles.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CachDependencyOnFiles.aspx.cs" Inherits="WebFormFirst.CachDependencyOnFiles" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Get countries" />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormFirst/CachDependencyOnFiles.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web.Caching;
+
+namespace WebFormFirst
+{
+    public partial class CachDependencyOnFiles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(Cache["Countries"]!=null)
+            {
+                DataSet ds = (DataSet)Cache["Countries"];
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                Label1.Text = ds.Tables[0].Rows.Count.ToString() + " retrieved from cach";
+            }
+            else
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/App_Data/countries.xml"));
+                Cache.Insert("Countries", ds, null, DateTime.Now.AddSeconds(20), Cache.NoSlidingExpiration);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                Label1.Text = ds.Tables[0].Rows.Count.ToString() + " retrieved from db";
+
+            }
+
+        }
+    }
+}
