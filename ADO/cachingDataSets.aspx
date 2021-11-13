96519f3c6f6237b2893a8451c6f8fe6393470ddb
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-13 13:36:57 +0200

    13/11

--- ADO/cachingDataSets.aspx
+++ ADO/cachingDataSets.aspx
@@ -1,5 +1,4 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" Trace="false" CodeBehind="cachingDataSets.aspx.cs" Inherits="ADO.cachingDataSets" %>
-<%@ OutputCache Duration="30" VaryByParam="None" %>
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
--- ADO/cachingDataSets.aspx.cs
+++ ADO/cachingDataSets.aspx.cs
@@ -4,6 +4,8 @@ using System.Data.SqlClient;
 using System.Data.SqlClient;
 using System.Data;
 using System.Configuration;
+using System.Web.Caching;
+using System.Collections.Generic;
 
 namespace ADO
 {
@@ -19,21 +21,69 @@ namespace ADO
 
         protected void LoadData_Click(object sender, EventArgs e)
         {
-            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
-            using (SqlConnection conn = new SqlConnection(cs))
+
+            if (Cache["tblemployees"] != null)
             {
-                SqlDataAdapter da = new SqlDataAdapter("select * from tblemployees", conn);
-                DataSet ds = new DataSet();
-                da.Fill(ds);
-                GridView1.DataSource = ds;
+                GridView1.DataSource = Cache["tblemployees"];
                 GridView1.DataBind();
-                Label1.Text = DateTime.Now.ToString();
+                Label1.Text = "retrive data from cach at " + DateTime.Now.ToString();
+            }
+            else
+            {
+                string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(cs))
+                {
+                    SqlCommand command = new SqlCommand("select * from tblemployees;select * from videos", conn);
+                    conn.Open();
+                    SqlDataReader dr = command.ExecuteReader();
+                    DataSet ds = new DataSet();
+
+                    List<DataTable> dtList = new List<DataTable>();
+                    DataTable t = new DataTable();
+                    t.Load(dr);
+                    dtList.Add(t);
+                    while (dr.NextResult())
+                    {
+                        DataTable table = new DataTable();
+                        table.Load(dr);
+                        dtList.Add(table);
+                    }
+                    
+                    ds.Tables.Add(dtList[0]);
+                    ds.Tables.Add(dtList[1]);
+
+                    GridView1.DataSource = ds.Tables[0];
+                    GridView1.DataBind();
+                    Label1.Text = "retrive data from db at "+ DateTime.Now.ToString();
+                    Cache["tblemployees"] = ds;
+
+                    SqlCacheDependencyAdmin.EnableNotifications(cs);
+                    SqlCacheDependencyAdmin.EnableTableForNotifications(cs, "tblemployees");
+
+                    SqlCacheDependency sqlCacheDependency = new SqlCacheDependency("firstdb", "tblemployees");
+                    CacheItemRemovedCallback cacheItemRemoved = new CacheItemRemovedCallback(cachItemRemoved);
+                    CacheItemUpdateCallback cacheItemUpdateCallback = new CacheItemUpdateCallback(cachItemUpdate);
+                    Cache.Insert("tblemployees", ds, sqlCacheDependency);
+                }
             }
         }
 
         protected void ClearCach_Click(object sender, EventArgs e)
         {
-            Response.Write("ahmed");
+            if (Cache["tblemployees"] != null)
+                Cache.Remove("tblemployees");
         }
+        private void cachItemRemoved(string key, object value, CacheItemRemovedReason reason)
+        {
+        }
+        private void cachItemUpdate(string key, CacheItemUpdateReason reason, out object expensiveObject, out CacheDependency dependency, out DateTime absoluteExpiration, out TimeSpan slidingExpiration)
+        {
+            expensiveObject = 1;
+            dependency = null;
+            absoluteExpiration  =Cache.NoAbsoluteExpiration;
+            slidingExpiration = Cache.NoSlidingExpiration;
+
+        }
+
     }
 }
