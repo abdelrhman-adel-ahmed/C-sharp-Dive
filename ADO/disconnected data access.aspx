bc2a2a06e2272eb3026c311970411d8c04e2190c
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-14 10:06:32 +0200

    14/11

---
+++ ADO/disconnected data access.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="disconnected data access.aspx.cs" Inherits="ADO.disconnected_data_access" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnGetDataFromDb" runat="server" Text="btnGetDataFromDb" OnClick="btnGetDataFromDb_Click" />
+            <br />
+            <asp:GridView ID="gvStudents" runat="server" AutoGenerateColumns="False" DataKeyNames="ID">
+                <Columns>
+                    <asp:CommandField ShowDeleteButton="True" ShowEditButton="True" />
+                    <asp:BoundField DataField="ID" HeaderText="ID" InsertVisible="False" ReadOnly="True" SortExpression="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" SortExpression="Gender" />
+                    <asp:BoundField DataField="TotalMarks" HeaderText="TotalMarks" SortExpression="TotalMarks" />
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Button ID="btnUpdateTable" runat="server" Text="btnUpdateTable" />
+            <br />
+            <asp:Label ID="lblStatus" runat="server" Text=""></asp:Label>
+            <br />
+           
+        </div>
+    </form>
+</body>
+</html>
---
+++ ADO/disconnected data access.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Caching;
+namespace ADO
+{
+    public partial class disconnected_data_access : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGetDataFromDb_Click(object sender, EventArgs e)
+        {
+            GetData();
+
+        }
+        private void GetData()
+        {
+            if(Cache["dataset"]!=null)
+            {
+                GetDataFromCach();
+            }
+            else
+            {
+                GetDataFromDb();
+            }
+        }
+        private void GetDataFromDb()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                string SelectQuery = "select * from tblStudents";
+                SqlDataAdapter da = new SqlDataAdapter(SelectQuery, conn);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds, "student");
+
+                ds.Tables["student"].PrimaryKey = new DataColumn[]
+                                            {ds.Tables["student"].Columns["ID"]};
+
+                Cache.Insert("dataset", ds,null,DateTime.Now.AddSeconds(18),Cache.NoSlidingExpiration);
+
+                gvStudents.DataSource = ds;
+                gvStudents.DataBind();
+
+
+
+                lblStatus.Text = "data loaded from db";
+
+            }
+        }
+        private void GetDataFromCach()
+        {
+            gvStudents.DataSource = Cache["dataset"];
+            gvStudents.DataBind();
+            lblStatus.Text = "data loaded from cach";
+
+        }
+    }
+}
