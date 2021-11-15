21848fb247eae5f8aab4ec00549a045ea4847b0d
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-15 09:46:16 +0200

    15/11

---
+++ ADO/typeVSuntypedDS.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ADO
+{
+    public partial class typeVSuntypedDS : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                string SelectQuery = "select * from tblStudents";
+                SqlDataAdapter da = new SqlDataAdapter(SelectQuery, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+        }
+    }
+}
