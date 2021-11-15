5e786e4c7aa1fae96aecda428778ee68f2783c00
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-15 13:53:10 +0200

    15/11

--- ADO/loadXmlDataIntoSqlTableUsingSqlbulkcopy.aspx.cs
+++ ADO/loadXmlDataIntoSqlTableUsingSqlbulkcopy.aspx.cs
@@ -26,8 +26,32 @@ namespace ADO
                 ds.ReadXml(Server.MapPath("~/App_Data/employee.xml"));
                 //the ds cotain two tables now Department and Employee ,the names comes from the schema of the xml
                 DataTable DtDept= ds.Tables["Department"];
+                DataTable DtEmp = ds.Tables["Employee"];
 
+                //we created two tables in the db Employees,Departmentss
+                conn.Open();
+                using (SqlBulkCopy bc= new SqlBulkCopy(conn))
+                {
+                    //first we need to specife the table name 
+                    //then we need to specife the sourceColumns Names and the destination columns Names
+                    bc.DestinationTableName = "Departmentss";
+                    bc.ColumnMappings.Add("ID", "ID");
+                    bc.ColumnMappings.Add("Name", "Name");
+                    bc.ColumnMappings.Add("Location", "Location");
+                    bc.WriteToServer(DtDept);
 
+                }
+                using (SqlBulkCopy bc = new SqlBulkCopy(conn))
+                {
+              
+                    bc.DestinationTableName = "Employees";
+                    bc.ColumnMappings.Add("ID", "ID");
+                    bc.ColumnMappings.Add("Name", "Name");
+                    bc.ColumnMappings.Add("Gender", "Gender");
+                    bc.ColumnMappings.Add("DepartmentId", "DepartmentId");
+                    bc.WriteToServer(DtEmp);
+
+                }
             }
         }
     }
