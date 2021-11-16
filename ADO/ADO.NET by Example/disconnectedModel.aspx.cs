86aa648b5388b5d39f2b13dc4065ab1f10d7a2a1
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-16 09:43:47 +0200

    16/11

--- ADO/ADO.NET by Example/disconnectedModel.aspx.cs
+++ ADO/ADO.NET by Example/disconnectedModel.aspx.cs
@@ -20,6 +20,19 @@ namespace ADO.ADO.NET_by_Example
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            string cs = ConfigurationManager.ConnectionStrings["firstdb"].ConnectionString;
+            SqlConnection conn = new SqlConnection(cs);
+            SqlDataAdapter da = new SqlDataAdapter("select * from employees", conn);
+            SqlCommandBuilder builder = new SqlCommandBuilder(da);
+            DataSet ds = new DataSet();
+            da.Fill(ds,"employees");
+           //ds.Tables["employees"].Constraints.Add("Empno_pk", ds.Tables["employees"].Columns["Id"], true);
+            DataRow row = ds.Tables["employees"].NewRow();
+            row["Name"] = TextBox1.Text;
+            row["Gender"] = TextBox2.Text;
+            row["Departmentid"] = TextBox3.Text;
+            da.Update(ds, "employees");
+
 
         }
     }
