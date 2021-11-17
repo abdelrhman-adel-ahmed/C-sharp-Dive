5be294a67e8fc66a2065471a1a29c97bf5906d6d
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-17 12:24:52 +0200

    17/11

--- LINQ/orderby.cs
+++ LINQ/orderby.cs
@@ -10,18 +10,18 @@ namespace LINQ
     {
         public static void run()
         {
+            //orderby : sort the result-set in ascending or descending order.default is ascending order
             IEnumerable<Customer> Customers = Db.GetCustomerList();
             var result = from c in Customers
-                         orderby c.ContactName
+                             //ascending on the country desc on the contact name 
+                         orderby c.Country ,c.ContactName descending
                          select c;
 
 
             foreach (var item in result)
             {
-                Console.WriteLine(item.CompanyName);
-                Console.WriteLine(item.ContactName);
-                Console.WriteLine(item.Country);
-                Console.WriteLine();
+                Console.WriteLine(item.Country+" "+item.ContactName);
+            
             }
         }
     }
