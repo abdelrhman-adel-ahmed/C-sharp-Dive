5386c36e299a46a25c043733cf849d7bf257a617
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-18 10:34:04 +0200

    18/11

--- ExpressTrees/Intro.cs
+++ ExpressTrees/Intro.cs
@@ -10,7 +10,14 @@ namespace ExpressTrees
     {
         public static void run()
         {
-            //.net doesnot know any thing about this c# syntax
+            /*
+             * .net doesnot know any thing about this c# syntax , .net can run any thing that is msil 
+             so before that we need to convert thast to syntax that ,net jit can compile
+            static bool ssdasdas(int i)
+            {
+            return i>5;
+            } ==== > this code when transelated to msil .net can understand it just  a function
+             */
             Func<int, bool> test = i => i > 5;
             Console.WriteLine(test(4));
             Console.WriteLine(test(6));
