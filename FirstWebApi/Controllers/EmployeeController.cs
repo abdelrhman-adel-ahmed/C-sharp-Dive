fc2f756a29081ac595de05822d96234687bece72
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-19 18:57:21 +0200

    19/11

---
+++ FirstWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAceess;
+using System.Data.Entity;
+
+namespace FirstWebApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            firstdbEntities obj = new firstdbEntities();
+            return obj.Employees.ToList();
+        }
+        public Employee Get(int id)
+        {
+            firstdbEntities obj = new firstdbEntities();
+            return obj.Employees.FirstOrDefault(e => e.ID == id);
+        }
+    }
+
+}
