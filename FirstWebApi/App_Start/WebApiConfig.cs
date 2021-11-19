9d27533d8bc30fd62c1082cc8544807e1425d393
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-19 20:40:23 +0200

    19/11

--- FirstWebApi/App_Start/WebApiConfig.cs
+++ FirstWebApi/App_Start/WebApiConfig.cs
@@ -19,6 +19,12 @@ namespace FirstWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional}
             );
+
+            //foramtter custom configrations 
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = 
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
         }
     }
 }
