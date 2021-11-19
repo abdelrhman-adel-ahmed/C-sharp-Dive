ebebea073182cace535cb06f46d5e6dac3e2b413
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-19 15:19:11 +0200

    19/11

--- ExpressTrees/Building Queries at Runtime.cs
+++ ExpressTrees/Building Queries at Runtime.cs
@@ -10,20 +10,32 @@ namespace ExpressTrees
 {
     class Building_Queries_at_Runtime
     {
-
+        delegate bool mydelegate();
         public static void run()
         {
             var zrbo = new MeContext2();
-            foreach (var item in zrbo.Employees)
-            {
-                Console.WriteLine(item.ID);
-                Console.WriteLine(item.Name);
-                Console.WriteLine(item.Gender);
-                Console.WriteLine(item.DepartmentId);
-                Console.WriteLine();
+            //foreach (var item in zrbo.Employees)
+            //{
+            //    Console.WriteLine(item.ID);
+            //    Console.WriteLine(item.Name);
+            //    Console.WriteLine(item.Gender);
+            //    Console.WriteLine(item.DepartmentId);
+            //    Console.WriteLine();
+            //}
+            IQueryable<Employees> emp = zrbo.Employees;
+            
+            Random randy = new Random();
+            mydelegate d = () => randy.Next() % 2 == 0;
 
 
-            }
+            Console.WriteLine(emp);
+            if (d())
+                emp = emp.Where(e => e.ID > 2);
+            if(d())
+                emp = emp.Where(e => e.Gender == "Female");
+
+            Console.WriteLine(emp);
+
         }
     }
     class MeContext2 : DbContext
