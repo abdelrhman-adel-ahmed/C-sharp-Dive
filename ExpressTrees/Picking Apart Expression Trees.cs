6031b9a7d5d60990e3eeaaae288c16830f41668e
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-19 10:39:55 +0200

    19/11

--- ExpressTrees/Picking Apart Expression Trees.cs
+++ ExpressTrees/Picking Apart Expression Trees.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,15 @@ namespace ExpressTrees
 {
     class Picking_Apart_Expression_Trees
     {
+
+       public static void run()
+        {
+            //Func<int, bool> del = i => i > 5;
+            //constant is factory method that create and return ConstantExpression object
+            ConstantExpression constExp = Expression.Constant(5, typeof(int));
+            Console.WriteLine(constExp.NodeType);
+            Console.WriteLine(constExp.Type);
+
+        }
     }
 }
--- ExpressTrees/Program.cs
+++ ExpressTrees/Program.cs
@@ -16,7 +16,11 @@ namespace ExpressTrees
             Intro.run();
 
             Console.WriteLine("-------------Expression_Trees--------------------");
-            Expression_Trees.run();            
+            Expression_Trees.run();
+
+            Console.WriteLine("-------------Picking_Apart_Expression_Trees--------------------");
+            Picking_Apart_Expression_Trees.run();
+
 
             Console.ReadLine();
         }
