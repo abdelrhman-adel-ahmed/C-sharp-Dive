f0e08f99281834580361a6e65ce4583bc45d70cf
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-20 15:21:07 +0200

    20/11

---
+++ EmpolyeeAuthServiceApi/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmpolyeeAuthServiceApi.Controllers
+{
+    [Authorize]
+    public class EmployeeController : ApiController
+    {
+
+        public IEnumerable<Employee> Get()
+        {
+            seconddbEntities db = new seconddbEntities();
+            return db.Employees.ToList();
+        }
+    }
+}
