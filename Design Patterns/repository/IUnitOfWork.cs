23ada71eee46042960e18677929ed78030c865bf
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-23 19:15:01 +0200

    23/11

--- Design Patterns/repository/IUnitOfWork.cs
+++ Design Patterns/repository/IUnitOfWork.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Design_Patterns.repository;
 
-namespace Design_Patterns.core.repository
+namespace Design_Patterns.core
 {
-    interface IUnitOfWork
+    interface IUnitOfWork:IDisposable
     {
-
-
+        ICourseRepository Course { get; }
+        int Complete();
     }
 }
