6b93746bcf8c3b896d7f2bb8e0288d88c96feaa3
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-25 11:22:06 +0200

    25/11

---
+++ EmpolyeeAuthServiceApi/Controllers/Custom/CustomControllerSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Dispatcher;
+
+namespace EmpolyeeAuthServiceApi.Controllers.Custom
+{
+    public class CustomControllerSelector:DefaultHttpControllerSelector
+    {
+        HttpConfiguration _configuration;
+        public CustomControllerSelector(HttpConfiguration configuration) :base(configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
