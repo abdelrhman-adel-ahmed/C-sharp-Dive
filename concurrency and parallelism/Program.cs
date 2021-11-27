bf8095efe2a6725afbabb11f499d822852df71fb
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-27 09:42:27 +0200

    27/11

--- concurrency and parallelism/Program.cs
+++ concurrency and parallelism/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Threading.Tasks;
 using concurrency_and_parallelism.Async;
-
+using concurrency_and_parallelism.Threading;
 namespace concurrency_and_parallelism
 {
     class Program
     {
       
-        static async Task Main(string[] args)
+        //static async Task Main(string[] args)
+        //{
+          //await AsyncMakeTea.run(); 
+        //}
+        static void Main(string[] args)
         {
-           await AsyncMakeTea.run(); 
+
+            ThreadOverHead.ThreadoverHead();
         }
-   
+
     }
 }
---
+++ concurrency and parallelism/Threading/ThreadOverHead.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace concurrency_and_parallelism.Threading
+{
+    class ThreadOverHead
+    {
+        ParameterizedThreadStart  d(ManualResetEvent m);
+        static void wakeThread(ManualResetEvent m) { }
+        
+        public static void ThreadoverHead()
+        {
+            d dd = wakeThread;
+            const int OneMB = 1024 * 1024;
+            using (ManualResetEvent wakeThread = new ManualResetEvent(false))
+            {
+                int ThreadNum = 0;
+                try
+                {
+                    while(true)
+                    {
+                        Thread t = new Thread(dd);
+                        t.Start(wakeThread);
+                        Console.WriteLine($"{++ThreadNum} {Process.GetCurrentProcess().VirtualMemorySize64/OneMB}MB");
+                    }
+                }
+                catch (OutOfMemoryException)
+                {
+                    Console.WriteLine($"out of memory after creating {ThreadNum}");
+                    Debugger.Break();
+                    wakeThread.Reset();
+                }
+            }
+        }
+    }
+}
