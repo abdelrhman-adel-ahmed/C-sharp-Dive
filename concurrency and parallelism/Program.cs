d7e88469bdf9fc47b731ce86fb56631bdd4e2731
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-28 10:01:44 +0200

    28/11

--- concurrency and parallelism/Program.cs
+++ concurrency and parallelism/Program.cs
@@ -14,16 +14,16 @@ namespace concurrency_and_parallelism
         static void Main(string[] args)
         {
 
-            //ThreadOverHead.ThreadoverHead();
+            // ThreadOverHead.ThreadoverHead();
 
-           // Console.WriteLine("----------------Hello_World_Thread--------------------");
-           // Hello_World_Thread.run();
+            // Console.WriteLine("----------------Hello_World_Thread--------------------");
+            // Hello_World_Thread.run();
 
-            //Console.WriteLine("----------------Sync--------------------");
-            //Sync.run();
+            // Console.WriteLine("----------------Sync--------------------");
+            // Sync.run();
 
-            //Console.WriteLine("----------------Difference_Between_Background_and_Foreground_Thread--------------------");
-            //Difference_Between_Background_and_Foreground_Thread.run();
+            // Console.WriteLine("----------------Difference_Between_Background_and_Foreground_Thread--------------------");
+            // Difference_Between_Background_and_Foreground_Thread.run();
 
             Console.WriteLine("----------------Basic_Thread_Synchronization--------------------");
             Basic_Thread_Synchronization.run();
