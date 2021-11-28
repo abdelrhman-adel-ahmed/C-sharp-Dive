d5c4211b5eed1fed03b2a94bceed61a8a8715543
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-11-28 10:14:32 +0200

    28/11

--- concurrency and parallelism/Threading/Thread Synchronization/AnotherSyncExample.cs
+++ concurrency and parallelism/Threading/Thread Synchronization/AnotherSyncExample.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace concurrency_and_parallelism.Threading
-{ 
+{
     class AnotherSyncExample
     {
+        static object zrbo = new object();
+        static Random rand = new Random();
+
         public static void run()
         {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(UseBathRoomStall).Start();
+            }
 
+        }
+
+        static void UseBathRoomStall()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " trying to obtain bathroom");
+            lock (zrbo)
+            {
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " obtained the lock");
+                Thread.Sleep(rand.Next(2000));
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " on my way to release the lock");
+            } 
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " on my way to exit");
         }
     }
 }
