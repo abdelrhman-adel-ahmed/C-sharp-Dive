2fb2758505b06c76cd9b46e592bd4ac0f53d7dfd
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-01 10:19:15 +0200

    12/1

--- concurrency and parallelism/Threading/Thread Synchronization/Producer Consumer Problem/ProducerConsumerImp1.cs
+++ concurrency and parallelism/Threading/Thread Synchronization/Producer Consumer Problem/ProducerConsumerImp1.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Threading;
 namespace concurrency_and_parallelism.Threading
+
+//we have multiple consumer that want to cooperate with each other to take numbers from a list and add them
 {
     class ProducerConsumerImp1
     {
-        //we have multiple consumer that want to cooperate with each other to take numbers from a list and add them
+        static Queue<int> queue = new Queue<int>();
+        static Random rand = new Random();
+        const int ThreadNum = 3;
+        //the capacity of the list will be the num of threads ,each thread will put the sum that it calcualted in the 
+        //Sums list 
+        static List<int> Sums = new List<int>(ThreadNum);
         public static void run()
+        {
+            ProduceNumers();
+        }
+
+        static void Consumer()
         {
 
         }
+        static void ProduceNumers()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                queue.Enqueue(rand.Next(10));
+                Thread.Sleep(rand.Next(1000));
+            }
+        }
+
     }
 }
