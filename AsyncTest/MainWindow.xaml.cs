8613b71b56280436d79a6037f4629eeed3f6ab4b
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-03 19:24:19 +0200

    3/12

--- AsyncTest/MainWindow.xaml.cs
+++ AsyncTest/MainWindow.xaml.cs
@@ -29,18 +29,36 @@ namespace AsyncTest
 
         private void NormalExec_Click(object sender, RoutedEventArgs e)
         {
+            //we have to problem with sync:
+            //1- we cannot do any thing with the UI till the event finish
+            //2-we can see the results all at once not in the order of there execution because the event fucntion
+            //is not returned 
             var watch = Stopwatch.StartNew();
             RunDownloadSync();
             watch.Stop();
             var timeTaken = watch.ElapsedMilliseconds;
             resultWindow.Text += $"Total time to of execution is: {timeTaken}";
-
-
         }
 
         private void AsyncExec_Click(object sender, RoutedEventArgs e)
         {
+            var watch = Stopwatch.StartNew();
 
+            RunDownloadAsync();
+            
+            watch.Stop();
+            var timeTaken = watch.ElapsedMilliseconds;
+            resultWindow.Text += $"Total time to of execution is: {timeTaken}";
+
+        }
+        private async Task RunDownloadAsync()
+        {
+            List<string> websites = PrepData();
+            foreach (var website in websites)
+            {
+                WebSiteDTO result = await Task.Run(() => DownLoadWebSiteAsync(website));
+                ReportWebSiteInfo(result);
+            }
         }
         private void RunDownloadSync()
         {
@@ -55,9 +73,19 @@ namespace AsyncTest
         private void ReportWebSiteInfo(WebSiteDTO result)
         {
             resultWindow.Text += $"{result.websiteUrl} Downloaded: {result.websiteData.Length} Charachters long. " +
-                $"{Environment.NewLine}";
+            $"{Environment.NewLine}";
         }
 
+        private WebSiteDTO DownLoadWebSiteAsync(string websiteUrl)
+        {
+            WebSiteDTO output = new WebSiteDTO();
+            WebClient clinet = new WebClient();
+
+            output.websiteUrl = websiteUrl;
+            output.websiteData = clinet.DownloadString(websiteUrl);
+
+            return output;
+        }
         private WebSiteDTO DownLoadWebSite(string websiteUrl)
         {
             WebSiteDTO output = new WebSiteDTO();
@@ -77,6 +105,7 @@ namespace AsyncTest
             output.Add("https://www.facebook.com");
             output.Add("https://www.google.com");
             output.Add("https://www.microsoft.com");
+            output.Add("https://www.stackoverflow.com");
 
             return output;
         }
