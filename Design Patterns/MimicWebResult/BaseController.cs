34ebfbbd2975c1034df33276e79021d018661b0a
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-07 19:08:12 +0200

    7/12

---
+++ Design Patterns/MimicWebResult/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns.MimicWebResult
+{
+    class BaseController
+    {
+        protected TOutput Execute<TOutput>(Func<Iprovider, TOutput> processDelegate,
+                                                               string genericErrorMessage, bool isTransaction = false)
+        {
+
+            TOutput output = baseDSL.Execute(processDelegate, genericErrorMessage, isTransaction);
+            return output;
+        }
+    }
+}
