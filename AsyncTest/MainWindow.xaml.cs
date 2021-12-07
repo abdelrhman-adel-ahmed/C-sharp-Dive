17ac8f0b53797931be0640440afebcf3328bd7b1
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-07 19:03:43 +0200

    7/12

--- AsyncTest/MainWindow.xaml.cs
+++ AsyncTest/MainWindow.xaml.cs
@@ -36,27 +36,37 @@ namespace AsyncTest
             //is not returned 
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             var watch = Stopwatch.StartNew();
-            List<WebSiteDTO> Results= HelperMethods.RunDownloadSync();
-
+            List<WebSiteDTO> Results = HelperMethods.RunDownloadSync();
+            ReportWebSiteInfo(Results);
             watch.Stop();
             var timeTaken = watch.ElapsedMilliseconds;
             resultWindow.Text += $"Total time to of execution is: {timeTaken} {Environment.NewLine}";
         }
+       public  async Task RunDownloadAsync(IProgress<string> Progress)
+        {
+            List<string> websites = HelperMethods.PrepData();
 
+            foreach (var website in websites)
+            {
+                WebSiteDTO result = await Task.Run(() => HelperMethods.DownLoadWebSiteAsync(website));
+                ReportWebSiteInfo(result);
+                Progress.Report()
+            }
+        }
         private async void AsyncExec_Click(object sender, RoutedEventArgs e)
         {
             ThreadNumTextBox.Text = "";
             var watch = Stopwatch.StartNew();
 
             ThreadNumTextBox.Text += $"async contoller before await thread id: {Thread.CurrentThread.ManagedThreadId} {Environment.NewLine}";
-            await HelperMethods.RunDownloadAsync();
+            await RunDownloadAsync();
             ThreadNumTextBox.Text += $"async contoller after await thread id: {Thread.CurrentThread.ManagedThreadId} {Environment.NewLine}";
 
             watch.Stop();
             var timeTaken = watch.ElapsedMilliseconds;
             resultWindow.Text += $"Total time to of execution is: {timeTaken}";
         }
-       
+
 
         private async void ParallelExecuteAsync_Click(object sender, RoutedEventArgs e)
         {
@@ -64,17 +74,28 @@ namespace AsyncTest
             var watch = Stopwatch.StartNew();
 
             ThreadNumTextBox.Text += $"async contoller before await thread id: {Thread.CurrentThread.ManagedThreadId} {Environment.NewLine}";
-            await HelperMethods.RunDownloadAsyncParallel();
+            var Results = await HelperMethods.RunDownloadAsyncParallel();
+            ReportWebSiteInfo(Results);
             ThreadNumTextBox.Text += $"async contoller after await thread id: {Thread.CurrentThread.ManagedThreadId} {Environment.NewLine}";
 
             watch.Stop();
             var timeTaken = watch.ElapsedMilliseconds;
             resultWindow.Text += $"Total time to of execution is: {timeTaken}";
         }
-        private void ReportWebSiteInfo(WebSiteDTO result)
+        private void ReportWebSiteInfo(List<WebSiteDTO> results)
         {
-            resultWindow.Text += $"{result.websiteUrl} Downloaded: {result.websiteData.Length} Charachters long. " +
-            $"{Environment.NewLine}";
+            resultWindow.Text = " ";
+            foreach (var result in results)
+            {
+                resultWindow.Text += $"{result.websiteUrl} Downloaded: {result.websiteData.Length} Charachters long. " +
+                        $"{Environment.NewLine}";
+            }
+
+        }
+        private  void ReportWebSiteInfo(WebSiteDTO result)
+        {
+                resultWindow.Text += $"{result.websiteUrl} Downloaded: {result.websiteData.Length} Charachters long. " +
+                        $"{Environment.NewLine}";
         }
     }
 }
---
+++ AsyncTest/ProgressReportModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsyncTest
+{
+    class ProgressReportModel
+    {
+        public int PercentageCompleted { get; set; } = 0;
+        public List<WebSiteDTO> SiteDownloaded { get; set; } = new List<WebSiteDTO>();
+    }
+}
