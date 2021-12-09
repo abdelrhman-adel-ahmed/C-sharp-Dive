f004c7fcc00154c72d934184dde34d77ae82d8c5
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-09 08:28:27 +0200

    9/12

--- Design Patterns/MimicWebResult/BaseDSL.cs
+++ Design Patterns/MimicWebResult/BaseDSL.cs
@@ -12,9 +12,13 @@ namespace Design_Patterns
         public TOutput Execute<TOutput>(Func<Iprovider, TOutput> processDelegate,
                                                          string genericErrorMessage, bool isTransaction = false)
         {
-          //1- get connection type 
-          //2- initialize connection factory 
-          //3- 
+            //1- get connection type 
+            //2- initialize connection factory 
+            //3- get connection object eaither oracle or sql using the connection factory we created 
+
+            //TOutput output = "aa"; --> error why ?!!!>
+            TOutput output = default(TOutput);
+            return output;
         }
 
     }
