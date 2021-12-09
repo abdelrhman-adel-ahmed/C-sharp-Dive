f3fffb6b58d24ed9eed88d5ec3c27ce34e86141a
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-09 08:16:43 +0200

    9/12

--- Design Patterns/Program.cs
+++ Design Patterns/Program.cs
@@ -9,57 +9,25 @@ using Design_Patterns.repository;
 namespace Design_Patterns
 {
 
-
-    class x
-    {
-        int lock_var = 0;
-        public void lock_()
-        {
-            if (lock_var == 0)
-            {
-                lock_var = 1;
-                return;
-            }
-
-            while (lock_var == 1)
-                continue;
-               
-        }
-
-            public void unlock_()
-            {
-
-            }
-        }
-
         class Program
         {
 
 
             static void Main(string[] args)
             {
-                int result = 0;
-                x xx = new x();
-                List<int> collection = new List<int>();
-                foreach (var item in collection)
-                {
-                    xx.lock_();
-                    result += 1;
-                    xx.unlock_();
-                }
-                //Console.WriteLine("---------MVC--------");
-                //EntryPoint.Start();
-                //Console.WriteLine("---------Dependency Inversion--------");
-                //Entrypoint1.run();
+
+            //Console.WriteLine("---------MVC--------");
+            //EntryPoint.Start();
+            //Console.WriteLine("---------Dependency Inversion--------");
+            //Entrypoint1.run();
 
 
 
-                //Console.WriteLine("---------Repository--------");
-                //Start.run();
-                int result = 0;
+            //Console.WriteLine("---------Repository--------");
+            //Start.run();
 
-                result += 1;
-                Console.ReadLine();
+            Console.WriteLine("---------Mimic Webresult--------");
+            StartUp.run();
             }
         }
     }
