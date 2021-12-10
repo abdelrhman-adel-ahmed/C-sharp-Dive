968b0214aee633adad55d6c35926e3f47486c391
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-10 12:38:29 +0200

    10/12

--- Design Patterns/MiddleWare/Soultion1.cs
+++ Design Patterns/MiddleWare/Soultion1.cs
@@ -6,8 +6,48 @@ using System.Threading.Tasks;
 
 namespace Design_Patterns
 {
+
+    public abstract class Pipe 
+    {
+        protected Action<string> _action;
+        public Pipe(Action<string> action)
+        {
+            _action = action;
+        }
+        public abstract void Handle(string msg);
+    }
+
+    public class Wrap : Pipe
+    {
+        public Wrap(Action<string> action) : base(action) { }
+        public override void Handle(string msg)
+        {
+            Console.WriteLine("start");
+            _action(msg);
+            Console.WriteLine("end");
+        }
+    }
+    public class Try : Pipe
+    {
+        public Try(Action<string> action) : base(action) { }
+        public override void Handle(string msg)
+        {
+            try
+            {
+                Console.WriteLine("trying");
+                _action(msg);
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+    }
+
+
     class Soultion1
     {
+        
         public static void First(string msg)
         {
             Console.WriteLine($"exc first func {msg}");
