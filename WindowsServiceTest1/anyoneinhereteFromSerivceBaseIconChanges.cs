9bd4fbee4ea79855b3abf8726cb66a0a4c11ad0d
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2021-12-20 00:06:59 +0200

    20/12

---
+++ WindowsServiceTest1/anyoneinhereteFromSerivceBaseIconChanges.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceTest1
+{
+    class anyoneinhereteFromSerivceBaseIconChanges : ServiceBase
+    {
+    }
+
+
+}
