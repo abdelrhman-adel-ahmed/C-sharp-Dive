0c564a119903ee35ea0c9457b2450f34be1905a3
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-03-04 14:40:39 +0200

    4/3

--- MutableAndImutableIndotNet/Program.cs
+++ MutableAndImutableIndotNet/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.ObjectModel;
 
 namespace MutableAndImutableIndotNet
 {
@@ -10,7 +12,10 @@ namespace MutableAndImutableIndotNet
     {
         static void Main(string[] args)
         {
+            var list = new List<string> { "1" }.AsReadOnly();
+            
 
         }
+
     }
 }
---
+++ MutableAndImutableIndotNetCore/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Immutable;
+namespace MutableAndImutableIndotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = ImmutableList.Create<string>("1");
+            list.Add("2");
+            list.ForEach(x => Console.WriteLine(x));
+
+        }
+    }
+}
