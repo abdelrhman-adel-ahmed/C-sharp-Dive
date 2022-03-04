17d90279a13ace4f41bf42a287f2a72485941542
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-03-04 14:27:42 +0200

    4/3

---
+++ benchMark/Bench.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+
+namespace benchMark
+{
+    [MemoryDiagnoser]
+    public class Bench
+    {
+        private static readonly Sample samples = new Sample();
+
+        [Benchmark]
+        public string GetNameDynamic()
+        {
+            return samples.GetNameDynamic();
+        }
+        [Benchmark]
+        public string GetName()
+        {
+            return samples.GetName();
+        }
+        [Benchmark]
+        public List<string> GetNameInListDynamic()
+        {
+            return samples.GetNameInListDynamic();
+        }
+        [Benchmark]
+        public List<string> GetNameInList()
+        {
+            return samples.GetNameInList();
+        }
+    }
+}
