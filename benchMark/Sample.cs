a8a455478e50beae026bff7afb8f6bda8ee62bcb
abdelrahman.adel <abdelrhman.adel@nt-me.com>
2022-03-04 14:24:44 +0200

    4/3

---
+++ benchMark/Sample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace benchMark
+{
+    public class User
+    {
+        public string Name { get; set; }
+    }
+
+    public class Sample
+    {
+        public readonly dynamic _valueDynamic = new User
+        {
+            Name = "zrbo"
+        };
+        public readonly User _value = new User
+        {
+            Name = "zrbo"
+        };
+
+        public string GetNameDynamic()
+        {
+            return _valueDynamic.Name;
+        }
+
+        public string GetName()
+        {
+            return _value.Name;
+        }
+
+        public List<string> GetNameInListDynamic()
+        {
+            return Enumerable.Range(0, 10000).Select(x => _valueDynamic.Name).Cast<string>().ToList();
+        }
+
+        public List<string> GetNameInList()
+        {
+            return Enumerable.Range(0, 10000).Select(x => _value.Name).ToList();
+        }
+    }
+}
