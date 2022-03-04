91eb23c7e92db094ed1896f1ee4b3080e293315f
abdelrahman.adel <abdelrhman.adel@nt-me.com>
2022-03-04 14:24:09 +0200

    3/4

---
+++ benchMark/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace benchMark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Bench>();
+            Console.ReadKey();
+        }
+    }
+}
