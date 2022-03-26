7ea10614ce60bd6ea1e7943d765b9a6a65387d9d
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-03-26 14:21:25 +0200

    26/3

--- MVVM using Caliburn Micro/ViewModels/ShellViewModel.cs
+++ MVVM using Caliburn Micro/ViewModels/ShellViewModel.cs
@@ -5,12 +5,45 @@ namespace MVVM_using_Caliburn_Micro.ViewModels
     public class ShellViewModel : Screen
     {
 
-        private string _fullName;
+        private string _firstName = "ahmed";
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName); //notify all people that using FullName that it has been changed
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+
+
+        private string _lastName = "mohamed";
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
 
         public string FullName
         {
-            get { return _fullName; }
-            set { _fullName = value; }
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
         }
     }
 }
