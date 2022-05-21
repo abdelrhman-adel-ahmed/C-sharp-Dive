a305fa02f9bb66d3372d206fc4398e53a8ded43c
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-05-21 10:47:00 +0200

    21/5

---
+++ ImageResizer/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageResizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BitmapImage dd = new BitmapImage();
+            dd.BeginInit();
+            dd.UriSource = new Uri(@"D:\XSP\111_1.jpeg");
+            dd.EndInit();
+            ImageBefore.Source = dd;
+
+        }
+    }
+}
