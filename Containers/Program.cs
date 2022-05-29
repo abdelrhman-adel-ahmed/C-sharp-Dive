a78b35d1a8b84bd4648ef9b16d7b9c49693fa9f7
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-05-29 15:18:39 +0200

    29/5

--- Containers/Program.cs
+++ Containers/Program.cs
@@ -4,6 +4,7 @@ using Containers_Ienumerable;
 using Containers_diff;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Containers
 {
@@ -43,23 +44,51 @@ namespace Containers
     {
         static readonly string DBPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory ?? "", @"UploaderDB.db");
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-        
+
             //Console.WriteLine("---------------arrays-------------------");
             //
             //arrays.run();
             //
-           Console.WriteLine("---------------IEnumeratorr-------------------");
-           IEnumeratorr.run();
-           
+            // Console.WriteLine("---------------IEnumeratorr-------------------");
+            // IEnumeratorr.run();
+            // 
             //Console.WriteLine("---------------IEnumerable_vs_IEnumerator-------------------");
             //IEnumerable_vs_IEnumerator.run();
             //
             //Console.WriteLine("---------------IEnumerable_vs_IEnumerator2-------------------");
             //IEnumerable_vs_IEnumerator2.run();
-
-
+            Console.WriteLine("7xGv44eFFXnQasXP7QH3xtNmkdqBAW5p".Length);
+            try
+            {
+                int x = await Rune();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("ahmed");
+        }
+        static Task<int> Rune()
+        {
+            Task<int> t;
+            try
+            {
+                t = Do();
+                Console.WriteLine("dsada");
+                return t;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return Do();
+        }
+        async static Task<int> Do()
+        {
+            await Task.Delay(2000);
+            throw new Exception("excpppppppppppppppppppp");
         }
     }
 }
