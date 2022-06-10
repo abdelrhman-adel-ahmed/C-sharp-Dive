69d48c1c9d4ca5dd59f4d90b2a986e5b46caa221
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-06-10 12:57:48 +0200

    10/6

--- CloudStorage/Program.cs
+++ CloudStorage/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,43 @@ namespace CloudStorage
     {
         static void Main(string[] args)
         {
-            MainDSL.FileUpload();
+            // MainDSL.FileUpload();
+            // Response<string> response =  zrboo("ahmed", Token());
+            while (true)
+            {
+                appsettings.zrb = ConfigurationManager.AppSettings["zrbo"];
+                Console.WriteLine(appsettings.zrb);
+            }
+        }
+
+        static string Token()
+        {
+            return "123";
+        }
+
+        static Response<T> zrboo<T>(T url, string token)
+        {
+            Console.WriteLine(token);
+            return new Response<T> { message = url };
+        }
+    }
+    class Response<T>
+    {
+        public T message { get; set; }
+    }
+    class appsettings
+    {
+        public static string zrb
+        {
+            get
+            {
+                return zrb;
+            }
+            set
+            {
+                zrb = value;
+                return;
+            }
         }
     }
 }
