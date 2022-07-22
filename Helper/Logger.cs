cd888a790da7466e342f20e2d2c09dc2125d4642
abdelrahman.adel <abdelrhman.bedo007@gmail.com>
2022-07-22 19:45:50 +0200

    22/7

---
+++ Helper/Logger.cs
@@ -0,0 +1,14 @@
+﻿namespace Helper
+{
+    public class Logger
+    {
+        public static void Zrbo()
+        {
+            System.Console.WriteLine("Zrbo");
+        }
+        public static void Elno()
+        {
+            System.Console.WriteLine("Elno");
+        }
+    }
+}
